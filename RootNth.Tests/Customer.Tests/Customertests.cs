7459fac242a5801e30b8bffbeb73e3fe23f0d5ec
MalchikovDmitry <dimamal1995@gmail.com>
2015-10-19 15:48:41 +0300

    Second task, but not completed

---
+++ RootNth.Tests/Customer.Tests/Customertests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Customer;
+
+namespace Customer.Tests
+{
+    public class CustomerTests
+    {
+
+        static Castomer c = new Castomer() { Name = "John", ContactPhone = "+375 33 465 774 34", Revenue = 10000 };
+
+
+       
+        [TestCase(c,"ncr",null, "Customer record: John, " )]
+        public void ToStringTest(Castomer cast, string format, IFormatProvider formatProvider, string result)
+        {
+
+        }
+    }
+}
---
+++ RootNth.Tests/Customer/Castomer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Customer
+{
+    public class Castomer : IFormattable
+    {
+        public string Name { get; set; }
+        public string ContactPhone { get; set; }
+        public decimal Revenue { get; set; }
+
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            string result = "Customer record: ";
+
+            if (format.Contains("n"))
+                result += Name.ToString(formatProvider);
+            if (format.Contains("c"))
+                result += ", " + ContactPhone.ToString(formatProvider);
+            if (format.Contains("r"))
+                result += ", " + Revenue.ToString(formatProvider);
+
+            return result;
+        }
+    }
+}
