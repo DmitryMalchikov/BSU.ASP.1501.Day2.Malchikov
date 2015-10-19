8d236e3e6b995138c99859b6b1b70e366f4cdbd7
MalchikovDmitry <dimamal1995@gmail.com>
2015-10-19 14:38:33 +0300

    Added Stein Algorithm

--- RootNth.Tests/GCDSearching.Tests/GCDSearcherTests.cs
+++ RootNth.Tests/GCDSearching.Tests/GCDSearcherTests.cs
@@ -6,6 +6,16 @@ namespace GCDSearching.Tests
 {
     public class GCDSearcherTests
     {
+        static object[] Cases =
+{
+            new object[] { 5,15,10,20,5},
+            new object[] { 7,13,17,19,1},
+            new object[] { 5,-15,10,-20,5},
+            new object[] { -5,-15,-10,-20,5},
+            new object[] { -5,-15,0,0,5},
+        };
+
+
         [TestCase(5, 15, 5)]
         [TestCase(7, 15, 1)]
         [TestCase(-5, 15, 5)]
@@ -14,7 +24,8 @@ namespace GCDSearching.Tests
         [TestCase(0, 0, 5, ExpectedException = typeof(ArgumentException))]
         public void GCDSearch_ForTwoArguments(int first, int second, int result)
         {
-            Assert.IsTrue(GCDSearching.GSDSearcher.FindGCD(first, second) == result);
+            long t;
+            Assert.IsTrue(GCDSearcher.FindGCD(first, second,out t) == result);
         }
 
         [TestCase(5, 15, 20, 5)]
@@ -26,23 +37,52 @@ namespace GCDSearching.Tests
         [TestCase(0, 0,0, 5, ExpectedException = typeof(ArgumentException))]
         public void GCDSearch_ForThreeArguments(int first, int second, int third, int result)
         {
-            Assert.IsTrue(GCDSearching.GSDSearcher.FindGCD(first, second, third) == result);
+            long t;
+            Assert.IsTrue(GCDSearcher.FindGCD(first, second, third, out t) == result);
         }
 
-        static object[] Cases =
-        {
-            new object[] { 5,15,10,20,5},
-            new object[] { 7,13,17,19,1},
-            new object[] { 5,-15,10,-20,5},
-            new object[] { -5,-15,-10,-20,5},
-            new object[] { -5,-15,0,0,5},
-        };
+
 
         [Test, TestCaseSource(nameof(Cases))]
         [TestCase(0,0,0,0,0,ExpectedException = typeof (ArgumentException))]
         public void GCDSearch_ForFourArguments(int a, int b, int c, int d, int result)
         {
-            Assert.IsTrue(GCDSearching.GSDSearcher.FindGCD(a,b,c,d) == result);
+            long t;
+            Assert.IsTrue(GCDSearcher.FindGCD(out t,a,b,c,d) == result);
+
+        }
+
+        [TestCase(5, 15, 5)]
+        [TestCase(7, 15, 1)]
+        [TestCase(-5, 15, 5)]
+        [TestCase(-5, -15, 5)]
+        [TestCase(0, 15, 15)]
+        [TestCase(0, 0, 5, ExpectedException = typeof(ArgumentException))]
+        public void SteinAlgorithm_ForTwoArguments(int first, int second, int result)
+        {
+            long t;
+            Assert.IsTrue(GCDSearcher.SteinAlgorithm(first, second, out t) == result);
+        }
+
+        [TestCase(5, 15, 20, 5)]
+        [TestCase(7, 15, 17, 1)]
+        [TestCase(-5, 15, 20, 5)]
+        [TestCase(-5, -15, -20, 5)]
+        [TestCase(0, 15, 7, 1)]
+        [TestCase(0, 5, 0, 5)]
+        [TestCase(0, 0, 0, 5, ExpectedException = typeof(ArgumentException))]
+        public void SteinAlgorithm_ForThreeArguments(int first, int second, int third, int result)
+        {
+            long t;
+            Assert.IsTrue(GCDSearcher.SteinAlgorithm(first, second, third, out t) == result);
+        }
+
+        [Test, TestCaseSource(nameof(Cases))]
+        [TestCase(0, 0, 0, 0, 0, ExpectedException = typeof(ArgumentException))]
+        public void SteinAlgorithm_ForFourArguments(int a, int b, int c, int d, int result)
+        {
+            long t;
+            Assert.IsTrue(GCDSearcher.SteinAlgorithm(out t, a, b, c, d) == result);
 
         }
     }
---
+++ RootNth.Tests/GCDSearching/GCDSearcher.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCDSearching
+{
+    public class GCDSearcher
+    {
+        public static int FindGCD(int first, int second, out long time)
+        {
+            time = 0;
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start();
+            if (first == 0 && second == 0) throw new ArgumentException();
+
+            if (first == 0) return second;
+            if (second == 0) return first;
+
+            if (first == second) return first;
+
+            first = Math.Abs(first);
+            second = Math.Abs(second);
+
+
+
+            while (first != 0 && second != 0)
+            {
+                if (first > second)
+                    first %= second;
+                else
+                    second %= first;
+            }
+
+            sw.Stop();
+
+            time = sw.ElapsedTicks;
+
+            return first + second;
+
+        }
+
+        public static int FindGCD(int first, int second, int third, out long time)
+        {
+            time = 0;
+            long bufTime;
+            int result = FindGCD(first, second, out bufTime);
+
+            time += bufTime;
+
+            result = FindGCD(result, third, out bufTime);
+
+            time += bufTime;
+
+            return result;
+        }
+
+        public static int FindGCD(out long time, params int[] numbers)
+        {
+            time = 0;
+            long bufTime;
+            int result = numbers[0];
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                result = FindGCD(result, numbers[i], out bufTime);
+                time += bufTime;
+            }
+
+            return result;
+        }
+
+        public static int SteinAlgorithm(int first, int second, out long time)
+        {
+            time = 0;
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start();
+
+            if (first == 0 && second == 0) throw new ArgumentException();
+
+            if (first == 0) return second;
+            if (second == 0) return first;
+
+            if (first == second) return first;
+
+            first = Math.Abs(first);
+            second = Math.Abs(second);
+
+            if (first == 1 || second == 1)
+            {
+                sw.Stop();
+                time = sw.ElapsedTicks;
+
+                return 1;
+            }
+
+            if (first % 2 == 0 && second % 2 == 0)
+                return 2 * SteinAlgorithm(first / 2, second / 2, out time);
+
+            if (first % 2 == 0)
+                return SteinAlgorithm(first / 2, second, out time);
+
+            if (second % 2 == 0)
+                return SteinAlgorithm(first, second / 2, out time);
+
+            if (first > second)
+                return SteinAlgorithm((first - second) / 2, second, out time);
+            if (second > first)
+                return SteinAlgorithm(first, (second - first) / 2, out time);
+            return 0;
+        }
+
+        public static int SteinAlgorithm(int first, int second, int third, out long time)
+        {
+            time = 0;
+            long bufTime;
+            int result = SteinAlgorithm(first, second, out bufTime);
+
+            time += bufTime;
+
+            result = SteinAlgorithm(result, third, out bufTime);
+
+            time += bufTime;
+
+            return result;
+        }
+
+        public static int SteinAlgorithm(out long time, params int[] numbers)
+        {
+            time = 0;
+            long bufTime;
+            int result = numbers[0];
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                result = SteinAlgorithm(result, numbers[i], out bufTime);
+                time += bufTime;
+            }
+
+            return result;
+        }
+
+    }
+}
